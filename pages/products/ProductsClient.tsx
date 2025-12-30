661c582c299dd298ec8ececb39ab2c3e86e3ec46
ramtin <ramtinkurd1387@gmail.com>
2025-12-29 16:57:45 -0800

    debug2

--- pages/products/ProductsClient.tsx
+++ pages/products/ProductsClient.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from "react"
-import { product } from "../../data/products"
+import { product } from "../data/products"
 import Link from "next/link"
 import Image from "next/image"
 
